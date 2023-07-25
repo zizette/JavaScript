ae92700ec5d284ff92eaba88b63e120d0c36e59c
Zizette <zchamaa@gmail.com>
2023-07-25 16:37:52 +0300

    task 3 done!

---
+++ task_3/script.js
@@ -0,0 +1,23 @@
+const num1 = 44;
+let num2 = 40;
+if (num1 >= num2) {
+
+    console.log("Hello Zizo ", num1, "is greater then", num2);
+}
+else {
+    console.log("Bye Zizo");
+    alert("Hello Zizo ", num2, "is greater then", num1);
+}
+num2 = 50
+canPass = true;
+if (canPass) {
+    if (num1 >= num2) {
+        console.log("Can Pass Hello Zizo ", num1, "is greater then", num2);
+    }
+    else {
+        alert("Can Pass Hello Zizo ");
+        console.log("Bye Zizo", num2, "is greater then", num1);
+
+    }
+}
+else { alert("try Again"); }
