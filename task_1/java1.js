87afdf27fd6e88360b3404bee660bbd269c9eb95
Zizette <zchamaa@gmail.com>
2023-07-25 14:51:10 +0300

    taks 1 done

---
+++ task_1/java1.js
@@ -0,0 +1,11 @@
+console.log("hye Zuzuzu");
+let isRaining = true;
+if (isRaining) {
+    alert("Coucou sunny");
+} else { console.log("It's Raining"); }
+
+let isShinning = false;
+isShinning ? (console.log(" Shinning Alsoo ")) : alert("not Shining!");
+
+
+
