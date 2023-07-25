a7b7ffae565e7ccb16f4e1a60f174af8faa0e03d
Zizette <zchamaa@gmail.com>
2023-07-25 15:27:13 +0300

    task2 done

---
+++ task_2/script.js
@@ -0,0 +1,11 @@
+let name;
+name = "Zizette Chamaa";
+name2 = "Lilianne Chamaa";
+
+do {
+    alert('Hello ' + name);
+    name = name2;
+    alert('Hello ' + name);
+    name = "0";
+}
+while (name = ! "0")
