d737bf5ce85c17d4d60a7abd29be8223eb3e0a0a
Zizette <zchamaa@gmail.com>
2023-07-26 14:36:05 +0300

    Task 5 done corr

--- task_5/main.js
+++ task_5/main.js
@@ -1,16 +1,14 @@
-
-const zarray_num = [55, 11, 222, 333, 666, 999, 800, 945, 961, 52];
+const numbers = [55, 11, 222, 333, 666, 999, 800, 945, 961, 52];
 conteur = 0;
 let znumb = 0;
 let zrest = 0;
-for (conteur; conteur <= zarray_num.length; conteur++) {
-    znumb = zarray_num[conteur];
-    // alert(zarray_num[conteur]);
-    console.log(zarray_num[conteur]);
+for (conteur; conteur < numbers.length; conteur++) {
+    znumb = numbers[conteur];
+    console.log(numbers[conteur]);
     zrest = znumb % 2;
     if (zrest == 0) {
-        // alert("even", znumb);
-        console.log("Here is an Even number : ", conteur, "  ", zarray_num[conteur]);
+
+        console.log("Here is an Even number : ", conteur, "  ", numbers[conteur]);
     }
-    else { console.log("Here is an Odd number : ", zarray_num[conteur]); }
+    else { console.log("Here is an Odd number : ", numbers[conteur]); }
 }
