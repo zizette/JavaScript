7d4a4bb3fb9e4ffb6acc037b9b20b0dab3a184c3
Zizette <zchamaa@gmail.com>
2023-07-26 11:09:35 +0300

    task 4 done

---
+++ task_4/main.js
@@ -0,0 +1,12 @@
+const zarr = [];
+console.log("Zarr ", zarr);
+const numbers = [0, 3.14, 9.81, 37, 98.6, 100]; // array of numbers
+console.log(numbers);
+alert(numbers);
+const zarray_num = [10, 50, 55, 15, 40];
+alert("my num array is " + zarray_num);
+alert("Numbers of elements in my zarray: " + zarray_num.length);
+const zarray_text = ["50", "40", "60", "30", "10"];
+const zarray_mixed = ["50", 40, "60", 30, "10"];
+console.log("mixed arrray", zarray_mixed)
+alert("mixed arrray" + zarray_mixed)
