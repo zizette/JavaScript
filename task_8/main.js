80d47a2f57c4f52c904c9647c95c6e13585c883e
Zizette <zchamaa@gmail.com>
2023-07-28 09:09:19 +0300

    task 8 done

--- task_8/main.js
+++ task_8/main.js
@@ -3,13 +3,14 @@ console.log(rndm);
 i = 0
 while (rndm <= 8) {
     i = i + 1;
-    console.log(rndm, "ith generation", i);
-    rndm = Math.floor(Math.floor() * 10);
+    console.log(rndm, "ith generation test with while random", i);
+    rndm = Math.floor(Math.random() * 10);
     console.log(rndm);
 }
-
+i = 0;
 // test do .... while
 do {
-    rndm = Math.floor(Math.floor() * 10);
-    console.log(rndm);
-} while (rndm <= 8)
+    ++i;
+    rndm = Math.floor(Math.random() * 10);
+    console.log(rndm, 'Random number ith : ', i);
+} while (rndm <= 8);
